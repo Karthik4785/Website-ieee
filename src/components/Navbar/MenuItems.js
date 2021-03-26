8c09296251be318be34c8517b107a34b7390d93c
Karthik Ambali <61372365+Karthik4785@users.noreply.github.com>
2021-03-26 15:01:15 +0530

    Navbar Added

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,28 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'About',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Execom',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Events',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact',
+        url: '#',
+        cName: 'nav-links'
+    },
+
+]
